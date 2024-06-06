f0433953650196cc5a6fec7b422fa7663bc724e6
NosredelG <glederson_cf@yahoo.com.br>
2024-06-06 15:07:06 -0300

    Operadores

---
+++ AprendendoASintaxeJava/src/edu/dio/operadores/Operadores.java
@@ -0,0 +1,144 @@
+package edu.dio.operadores;
+
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+        //Atribuicao
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+        //Aritimeticos
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //concatenacao
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+                
+        //qual o resultado das expressoes abaixo?
+        String concatenacao = "?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+        //Unarios
+        int numero = 5;
+                
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+        //Imprimindo o numero negativo
+        numero = -numero;
+        System.out.println(numero);
+
+        //tentando imprimir o numero positivo
+        System.out.println(+numero);
+
+        //tentando imprimir o numero positivo
+        numero = +numero;
+        System.out.println(numero);
+
+        //Imprimindo o numero positivo
+        numero = numero * -1;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
+
+        // ternario
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        */
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String valor = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(valor);
+
+        //classe Operadores.java
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.println("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.println("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.println("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.println("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.println("Numero 1 é diferente de numero 2");
+
+        String nomeUm = "Gleder";
+        String nomeDois = new String("Gleder");
+
+        System.out.println(nomeUm == nomeDois);
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        // Logicos
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
+    }   
+
+}
